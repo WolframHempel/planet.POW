c3838a3a62ee8eb0bd048e71f7220fb757991dfe
Shikaga <shikaga@gmail.com>
2013-06-01 15:14:56 +0200

    Coding standards

--- src/Player.js
+++ src/Player.js
@@ -1,18 +1,19 @@
-pp.Player = function( world, aiClass )
+pp.Player = function( _oWorld, cAI )
 {
-	this._oWorld = world;
+	this._oWorld = _oWorld;
 	this._oWorld.on( "update", this.receiveData, this );
 
 	this._oTank = new pp.object.Tank();
 	this._oWorld.add( this._oTank );
-	this._oAI = new aiClass(this);
+	this._oAI = new cAI(this);
 }
 
-pp.Player.prototype.receiveData = function( data )
+pp.Player.prototype.receiveData = function( _mData )
 {
-	this._oAI.receiveData( data );
+	this._oAI.receiveData( _mData );
 }
-pp.Player.prototype.receiveInstruction = function( instructions )
+
+pp.Player.prototype.receiveInstruction = function( _mInstructions )
 {
-	this._oTank.setInstructions( instructions.tank );
+	this._oTank.setInstructions( _mInstructions.tank );
 }
